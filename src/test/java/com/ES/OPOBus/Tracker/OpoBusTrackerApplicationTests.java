cb9b95fec4334af6203459d4c525f0fe7870687b
danielteixeira <danielteixeira31@ua.pt>
2020-04-08 17:29:28 +0100

    Esqueleto SpringBoot

---
+++ src/test/java/com/ES/OPOBus/Tracker/OpoBusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ES.OPOBus.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpoBusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
