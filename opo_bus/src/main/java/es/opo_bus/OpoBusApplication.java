416980f03211b552ae9956d87b2e9694f69a753a
danielteixeira <danielteixeira31@ua.pt>
2020-04-08 21:37:05 +0100

    new rest api structure - fixed error and added swagger dependency

---
+++ opo_bus/src/main/java/es/opo_bus/OpoBusApplication.java
@@ -0,0 +1,13 @@
+package es.opo_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpoBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpoBusApplication.class, args);
+    }
+
+}
---
+++ opo_bus/src/test/java/es/opo_bus/OpoBusApplicationTests.java
@@ -0,0 +1,13 @@
+package es.opo_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpoBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
