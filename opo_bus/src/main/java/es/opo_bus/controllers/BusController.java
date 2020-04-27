e89354d799bf2b36dad93699588b936dba0bba4d
danielteixeira <danielteixeira31@ua.pt>
2020-04-27 09:34:52 +0100

    cors

--- opo_bus/src/main/java/es/opo_bus/controllers/BusController.java
+++ opo_bus/src/main/java/es/opo_bus/controllers/BusController.java
@@ -7,6 +7,7 @@ import es.opo_bus.repositories.BusRepository;
 import es.opo_bus.repositories.RecordsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,18 +24,19 @@ public class BusController {
     @Autowired
     private RecordsRepository recordsRepository;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/bus")
     public @ResponseBody Iterable<Bus> listBuses() {
         return busRepository.findAll();
     }
 
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/bus/{id}")
     public @ResponseBody Optional<Bus> getBus(@PathVariable("id") String id) {
         return busRepository.findById(id);
     }
 
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/bus/lastRecord/{id}")
     public @ResponseBody Record getBusLocation(@PathVariable("id") String id) {
         Bus bus = busRepository.getOne(id);
@@ -42,7 +44,7 @@ public class BusController {
         return records.get(records.size()-1);
     }
 
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/records")
     public @ResponseBody Iterable<Record> listRecords() {
         return recordsRepository.findAll();
